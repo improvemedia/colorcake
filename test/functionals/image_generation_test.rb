94f9a2deda3df9552faaea15f33722d845e9b020
Victor Maslov <vm@improvemedia.ru>
2015-01-30 19:11:39 +0300

    sample images added; html reports generated; test parallelized and took colors from current marvin initializer; method profiler gem deprecated

--- test/functionals/image_generation_test.rb
+++ test/functionals/image_generation_test.rb
@@ -1,58 +1,53 @@
 require_relative '../../lib/colorcake'
-require 'method_profiler'
 
-Colorcake.configure {}
-def run
-  files = Array(0..15)
-  files.each_with_index do |file, index|
-    puts "image - #{index}"
-    @new_palette = []
-    @old_palette = {}
-    some = '<div id="colors">'
-    some += "<h1 style='clear:both'>Original Colors</h1>"
-    @finded_colors = Colorcake.extract_colors(Dir.pwd + "/fixtures/#{index}.jpg")
-    # colors = create_palette(@finded_colors)
-    # ap colors
-    colorspace_test = ''
-    @finded_colors[1].each do |color, percentage|
-      # puts color.inspect
-      # puts percentage.inspect
-      # if color == '#000000' && percentage[1].round(2) <= 5
-      # elsif percentage[1].round(2) >= 0.5
-        colorspace_test += "<div style='background: #{color}'></div>"
-      # end
+Colorcake.configure do
+  # taken from marvin initializer
+  @base_colors ||= %w{ 660000 cc0000 ea4c88 993399 663399 304961 0066cc 66cccc 77cc33 336600 cccc33 ffcc33 fff533 ff6600 c8ad7f 996633 663300 000000 999999 cccccc ffffff }
+end
+
+STDOUT.sync = true
+
+(0..15).map do |i|
+  Thread.new do
+    index = i
+    # puts "<< #{index}"
+
+    html = ""
+
+    found_colors = Colorcake.extract_colors File.join(__dir__, "fixtures", "#{index}.jpg")
+
+    found_colors[1].each do |color, percentage|
+      html << "<div style='background: #{color}'></div>"
     end
-    some += colorspace_test
-    some += "<h1 style='clear:both'>Search Colors</h1>"
+    html << "<h1 style='clear:both'>Search Colors</h1>"
 
-
-    colorspace_test = ""
-
-    @finded_colors[0].each do |color|
-      if color[1][:hex_of_base] == 'ffffff'
-        colorspace_test += "<div style='background: ##{color[1][:hex_of_base]};color:#333; border:1px solid red'>#{color[1][:search_factor]}</div>"
+    found_colors[0].each do |color|
+      html << if color[1][:hex_of_base] == "ffffff"
+        "<div style='background: ##{color[1][:hex_of_base]};color:#333; border:1px solid red'>#{color[1][:search_factor]}</div>"
       else
-        colorspace_test += "<div style='background: ##{color[1][:hex_of_base]};color:#fff;' >  #{color[1][:search_factor]} </div>"
+        "<div style='background: ##{color[1][:hex_of_base]};color:#fff;' >  #{color[1][:search_factor]} </div>"
       end
     end
-    some += colorspace_test
-    some += "<h1 style='clear:both'>Palette Colors(original 5 most contrast and colorful colors)</h1>"
 
-    colors = Colorcake.create_palette(@finded_colors[1])
-    # # ap colors
-    new_colors_test = ''
-     colors.sort_by{|x| x[1][1]}.reverse_each do |color, percentage|
-       new_colors_test += "<div style='background: #{color};color:#fff;'  >  #{percentage[1].round(2)} </div>"
-     end
-    some += new_colors_test
-    some = "<html><head><style>h1{font-size:16px}#colors{float:left; width: 50%}*{box-sizing:border-box}div{float:left;width:70px;height:70px;line-height:70px; text-align:center; font-weight:bold; font-family: Helvetica, Arial}</style></head><body>" + some + "</div><div id='image'><img style='display:block; clear:both' src='#{index}.jpg'/></div></body></html>"
-    #puts some
-    puts 'OK'
-    File.open("fixtures/photo#{file}.html", 'w') do |f|
-      f.write some
+    html << "<h1 style='clear:both'>Palette Colors(original 5 most contrast and colorful colors)</h1>"
+    Colorcake.create_palette(found_colors[1]).sort_by{ |x| - x[1][1] }.each do |color, percentage|
+      html << "<div style='background: #{color};color:#fff;'  >  #{percentage[1].round(2)} </div>"
     end
+
+    puts [$AAA, $BBB, $CCC, $DDD].inspect if ENV["DEBUG"]
+
+    File.open(File.join("fixtures", "photo#{index}.html"), "w") do |f|
+      f.write "<html><head><style>"\
+                 "h1{font-size:16px}"\
+                 "#colors{float:left; width: 50%}"\
+                 "*{box-sizing:border-box}"\
+                 "div{float:left;width:70px;height:70px;line-height:70px; text-align:center; font-weight:bold; font-family: Helvetica, Arial}"\
+               "</style></head>"\
+               "<body><div id=\"colors\"><h1 style='clear:both'>Original Colors</h1>" +
+                 html + "</div><div id='image'><img style='display:block; clear:both' src='#{index}.jpg'/></div>"\
+               "</body></html>"
+    end
+
+    # puts ">> #{index}"
   end
-end
-profiler = MethodProfiler.observe(Colorcake)
-run
-puts profiler.report
+end.map(&:join)
