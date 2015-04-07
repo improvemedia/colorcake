bea32d4eed2569a3cb606bb3f8870c4a92e5a0d5
Victor Maslov <vm@improvemedia.ru>
2015-04-07 19:59:42 +0300

    huge refactoring step 2

--- lib/colorcake.rb
+++ lib/colorcake.rb
@@ -85,61 +85,70 @@ module Colorcake
   def self.extract_colors src
     colors = {}
     colors_hex = {}
-    palette = compute_palette(src)
+
+    image = ::Magick::ImageList.new(src)
+    image_quantized = image.quantize(@colors_count, Magick::YIQColorspace)
+    palette = image_quantized.color_histogram # {#<Magick::Pixel:0x007fc19a08fd00>=>61660, ...}
+    image_quantized.destroy!
+    image.destroy!
 
     sum_of_pixels = palette.values.inject(:+)
     palette.each do |k, v|
-      palette[k] = [v, v / (sum_of_pixels.to_f / 100)]
+      palette[k] = [v, v / (sum_of_pixels / 100.0)]
     end
 
-    old_palette = palette
-
     new_palette = palette.map.with_index do |(color1, n1), index|
+      raise "Unexpected for algorithm: index < palette.length" unless index < palette.length
       common_colors = []
+      sr = color1.red
+      sb = color1.blue
+      sg = color1.green
+      sr /= 257 if sr / 255 > 0
+      sb /= 257 if sb / 255 > 0
+      sg /= 257 if sg / 255 > 0
+      lab = ColorUtil.rgb_to_lab([sr, sb, sg])
       if index < palette.length
         palette.each do |color2, n2|
-          sr = color1.red
-          sb = color1.blue
-          sg = color1.green
           cr = color2.red
           cb = color2.blue
           cg = color2.green
-          sr = color1.red / 257 if color1.red / 255 > 0
-          sb = color1.blue / 257 if color1.blue / 255 > 0
-          sg = color1.green / 257 if color1.green / 255 > 0
-          cr = color2.red / 257 if color2.red / 255 > 0
-          cb = color2.blue / 257 if color2.blue / 255 > 0
-          cg = color2.green / 257 if color2.green / 255 > 0
-          delta =  ColorUtil.delta_e(ColorUtil.rgb_to_lab([sr, sb, sg]),
-                                     ColorUtil.rgb_to_lab([cr, cb, cg]))
-          if delta < @delta
-            common_colors << [color2, n2]
-            common_colors << [color1, n1]
-            common_colors.uniq!
-
-            if common_colors.first[1][1] && common_colors.first[1][1] != n2[1]
-              common_colors.first[1][1] += n2[1]
-            elsif common_colors.first[1][1] == n2[1]
-              common_colors.first[1][1] = n2[1]
-            end
+          cr /= 257 if color2.red / 255 > 0
+          cb /= 257 if color2.blue / 255 > 0
+          cg /= 257 if color2.green / 255 > 0
+          next unless @delta > ColorUtil.delta_e(lab, ColorUtil.rgb_to_lab([cr, cb, cg]))
+          common_colors << [color2, n2]
+          common_colors << [color1, n1]
+          common_colors.uniq!
+          if common_colors.first[1][1] && common_colors.first[1][1] != n2[1]
+            common_colors.first[1][1] += n2[1]
           end
         end
         common_colors.uniq!
-        common_colors.each_with_index do |col, ind|
-          if ind != 0
-            old_palette.delete col[0]
-          end
+        common_colors.drop(1).each do |col|
+          palette.delete col[0]
         end
         common_colors.first
       end
     end
 
-    new_palette.each do |i|
-      c = i[0].to_s.split(',').map{ |x| x[/\d+/] }
-      b = compute_b(c)
+    new_palette.each do |color, n|
+      c = color.to_s.split(',').map{ |x| x[/\d+/] }
+
+      c.pop
+      c[0], c[1], c[2] = [c[0], c[1], c[2]].map do |s|
+        s = s.to_i
+        s = s / 257 if s / 255 > 0 # not all ImageMagicks are created equal....
+        s = s.to_s(16)
+        if s.size == 1
+          '0' + s
+        else
+          s
+        end
+      end
+      b = c.join.scan(/../).map{ |color| color.to_i 16 }
+
       closest_color = closest_color_to(b)
-      percentage = i[1][1]
-      colors_hex['#' + c.join] = i[1]
+      colors_hex['#' + c.join] = n
 
       id = if defined? Rails
         SearchColor.find_or_create_by_color(closest_color[0]).id
@@ -150,11 +159,11 @@ module Colorcake
       colors[id] ||= {}
       colors[id][:search_color_id] ||= id
       colors[id][:search_factor] ||= []
-      colors[id][:search_factor] << percentage
+      colors[id][:search_factor] << n[1]
       colors[id][:distance] ||= []
       colors[id][:hex] ||= c.join
       colors[id][:original_color] ||= []
-      colors[id][:original_color] << {('#' + c.join) => i[1]}
+      colors[id][:original_color] << {('#' + c.join) => n}
       colors[id][:hex_of_base] ||= @base_colors[id] if id
       colors[id][:distance] = closest_color[1] if colors[id][:distance] == []
     end
@@ -173,7 +182,27 @@ module Colorcake
 
   def self.create_palette colors
     if colors.length > @max_numbers_of_color_in_palette
-      colors = slim_palette colors
+
+      col_array = colors.to_a
+      matrix = col_array.map do |row|
+        col_array.map do |col|
+          rgb_color_1 = ColorUtil.rgb_from_string(row[0])
+          rgb_color_2 = ColorUtil.rgb_from_string(col[0])
+          pixel_1 = ColorUtil.rgb_to_lab([rgb_color_1[0], rgb_color_1[1], rgb_color_1[2]])
+          pixel_2 = ColorUtil.rgb_to_lab([rgb_color_2[0], rgb_color_2[1], rgb_color_2[2]])
+          diff = ColorUtil.delta_e(pixel_1, pixel_2)
+          # c1 = ColorUtil.rgb_to_hcl(rgb_color_1[0],rgb_color_1[1],rgb_color_1[2])
+          # c2 = ColorUtil.rgb_to_hcl(rgb_color_2[0],rgb_color_2[1],rgb_color_2[2])
+          # diff = ColorUtil.distance_hcl(c1, c2)
+          diff == 0 ? 100_000 : diff
+        end
+      end
+      colors_position = find_position_in_matrix_of_closest_color(matrix)
+      closest_colors = [colors.to_a[colors_position[0]], colors.to_a[colors_position[1]]]
+      merge_result = MergeColorsMethods.lab_merge(closest_colors)
+      colors.merge!(merge_result[0])
+      colors.delete(merge_result[1])
+
       create_palette colors
     elsif colors.length == @max_numbers_of_color_in_palette
       return colors
@@ -185,31 +214,17 @@ module Colorcake
 
   private
 
-  def self.compute_b c
-    c.pop
-    c[0], c[1], c[2] = [c[0], c[1], c[2]].map do |s|
-      s = s.to_i
-      s = s / 257 if s / 255 > 0 # not all ImageMagicks are created equal....
-      s = s.to_s(16)
-      if s.size == 1
-        '0' + s
-      else
-        s
-      end
-    end
-    c.join.scan(/../).map{ |color| color.to_i 16 }
-  end
-
   def self.closest_color_to b
     # do not remove, used in /marvin/lib/tasks/colors.rake
-    closest_colors = {}
-    @cluster_colors.each_key do |extended_color, |
-      closest_colors[extended_color] = ColorUtil.delta_e(
-        ColorUtil.rgb_to_lab(ColorUtil.rgb_number_from_string(extended_color)),
-        ColorUtil.rgb_to_lab(b)
-      )
-    end
-    closest_color = closest_colors.min_by{ |a, d| d }
+    closest_color = @cluster_colors.map do |extended_color, |
+      [
+        extended_color,
+        ColorUtil.delta_e(
+          ColorUtil.rgb_to_lab(ColorUtil.rgb_number_from_string(extended_color)),
+          ColorUtil.rgb_to_lab(b)
+        )
+      ]
+    end.min_by{ |color, delta| delta }
     if cluster = @cluster_colors[closest_color[0]]
       closest_color = [
         cluster,
@@ -222,21 +237,13 @@ module Colorcake
     closest_color
   end
 
-  def self.compute_palette src
-    image = ::Magick::ImageList.new(src)
-    image_quantized = image.quantize(@colors_count, Magick::YIQColorspace)
-    palette = image_quantized.color_histogram # {#<Magick::Pixel:0x007fc19a08fd00>=>61660, ...}
-    image_quantized.destroy!
-    image.destroy!
-    palette
-  end
-
   def self.expand_palette colors
     col_array = colors.to_a
     rgb_color_1 = ColorUtil.rgb_from_string(col_array[0][0])
-    rgb_color_2 = ColorUtil.rgb_from_string(col_array[-1][0])
-    if col_array.length == 1
-      rgb_color_2 = [
+    rgb_color_2 = if col_array.length != 1
+      ColorUtil.rgb_from_string(col_array[-1][0])
+    else
+      [
         rgb_color_1[0] + rand(0..10),
         rgb_color_1[1] + rand(0..20),
         rgb_color_1[2] + rand(0..30),
@@ -251,34 +258,10 @@ module Colorcake
     colors.merge!( { '#' + rgb.join => [1, 2] } )
   end
 
-  def self.slim_palette colors
-    col_array = colors.to_a
-    matrix = col_array.map do |row|
-      col_array.map do |col|
-        rgb_color_1 = ColorUtil.rgb_from_string(row[0])
-        rgb_color_2 = ColorUtil.rgb_from_string(col[0])
-        pixel_1 = ColorUtil.rgb_to_lab([rgb_color_1[0], rgb_color_1[1], rgb_color_1[2]])
-        pixel_2 = ColorUtil.rgb_to_lab([rgb_color_2[0], rgb_color_2[1], rgb_color_2[2]])
-        diff = ColorUtil.delta_e(pixel_1, pixel_2)
-        # c1 = ColorUtil.rgb_to_hcl(rgb_color_1[0],rgb_color_1[1],rgb_color_1[2])
-        # c2 = ColorUtil.rgb_to_hcl(rgb_color_2[0],rgb_color_2[1],rgb_color_2[2])
-        # diff = ColorUtil.distance_hcl(c1, c2)
-        diff == 0 ? 100_000 : diff
-      end
-    end
-    colors_position = find_position_in_matrix_of_closest_color(matrix)
-    closest_colors = [colors.to_a[colors_position[0]], colors.to_a[colors_position[1]]]
-    merge_result = MergeColorsMethods.lab_merge(closest_colors)
-    colors.merge!(merge_result[0])
-    colors.delete(merge_result[1])
-    colors
-  end
-
   def self.find_position_in_matrix_of_closest_color matrix
-    matrix_array = matrix.to_a
-    minimum = matrix_array.flatten.min
-    i = matrix_array.index{ |x| x.include? minimum }
-    [i, matrix_array[i].index(minimum)]
+    flattened = matrix.flatten
+    i = flattened.index flattened.compact.min
+    [i / matrix.size, i % matrix.size]
   end
 
 end
--- test/functionals/README.md
+++ test/functionals/README.md
@@ -6,7 +6,7 @@
 
 Если rmagick не будет билдится, можно поставить предыдущую версию как-то так: `sudo gem install rmagick -v 2.13.3`.
 
-`ruby image_generation_test.rb` -- генерирует шестнадцать html-отчетов по картинкам из /fixtures с использованием функций `extract_colors` и `create_palette`
+`ruby image_generation_test.rb` -- генерирует шестнадцать html-отчетов по картинкам из /fixtures с использованием функций `extract_colors` и `create_palette`  
 `ruby refactoring_test.rb` -- прогоняет image_generation_test и ругается, если хеш-суммы изменились.
 
 Текущая `#expand_palette` бажна и рандомна, поэтому мы не будем класть в `/fixtures` картинки с маленьким набором цветов, пока не перейдем от стадии осторожного рефакторинга к исправлению работы и улучшению библиотеки.
